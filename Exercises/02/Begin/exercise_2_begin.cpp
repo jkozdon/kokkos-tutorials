c338679d999a0e5476ba4f9912f389d90dac05cc
Jeremy E Kozdon <jeremy.kozdon@nextsilicon.com>
2023-04-20 04:07:21 +0000

    Exercise 02 solution
    Signed-off-by: Jeremy E Kozdon <jeremy.kozdon@nextsilicon.com>

--- Exercises/02/Begin/exercise_2_begin.cpp
+++ Exercises/02/Begin/exercise_2_begin.cpp
@@ -79,37 +79,37 @@ int main(int argc, char *argv[]) {
     // EXERCISE: Create views of the right size.
 
     // 1. Device Views
-    // typedef Kokkos::View<double*>   ViewVectorType;
-    // typedef Kokkos::View<double**>  ViewMatrixType;
-    // ViewVectorType y( "y", N );
-    // ViewVectorType x( "x", M );
-    // ViewMatrixType A( "A", N, M );
+    typedef Kokkos::View<duuble *> ViewVectorType;
+    typedef Kokkos::View<double **> ViewMatrixType;
+    ViewVectorType y("y", N);
+    ViewVectorType x("x", M);
+    ViewMatrixType A("A", N, M);
 
     // EXERCISE: This no longer needs allocation after views introduced...
     //   Hint: If arrays are not allocated, they also do not need to be
     //   deallocated below
     // Allocate y, x vectors and Matrix A:
-    double *const y = new double[N];
-    double *const x = new double[M];
-    double *const A = new double[N * M];
+    // double *const y = new double[N];
+    // double *const x = new double[M];
+    // double *const A = new double[N * M];
 
     // Initialize y vector on host.
     // EXERCISE: Convert y to 1D View's member access API: x(i)
     for (int i = 0; i < N; ++i) {
-      y[i] = 1;
+      y(i) = 1;
     }
 
     // Initialize x vector on host.
     // EXERCISE: Convert x to 1D View's member access API: x(i)
     for (int i = 0; i < M; ++i) {
-      x[i] = 1;
+      x(i) = 1;
     }
 
     // Initialize A matrix on host, note 2D indexing computation.
     // EXERCISE: convert 'A' to use View's member access API: A(j,i)
     for (int j = 0; j < N; ++j) {
       for (int i = 0; i < M; ++i) {
-        A[j * M + i] = 1;
+        A(j, i) = 1;
       }
     }
 
@@ -127,10 +127,10 @@ int main(int argc, char *argv[]) {
 
             // EXERCISE: Replace access with view access operators.
             for (int i = 0; i < M; ++i) {
-              temp2 += A[j * M + i] * x[i];
+              temp2 += A(j, i) * x(i);
             }
 
-            update += y[j] * temp2;
+            update += y(j) * temp2;
           },
           result);
 
@@ -162,9 +162,9 @@ int main(int argc, char *argv[]) {
         "bandwidth( %g GB/s )\n",
         N, M, nrepeat, Gbytes * 1000, time, Gbytes * nrepeat / time);
 
-    delete[] y;  // EXERCISE hint: ...
-    delete[] x;  // EXERCISE hint: ...
-    delete[] A;  // EXERCISE hint: ...
+    // delete[] y;  // EXERCISE hint: ...
+    // delete[] x;  // EXERCISE hint: ...
+    // delete[] A;  // EXERCISE hint: ...
   }
   Kokkos::finalize();
 
